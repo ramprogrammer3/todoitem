c72ae2bbdfd812779bb860dfb61829c19b3cbd9f
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-09-18 15:28:09 +0530

    todoItem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react"
+const App = () => {
+  const [name, setName] = useState("");
+  const [todoItem, setTodoItem] = useState([]);
+  const addItem = () =>{
+    let copyTodoItem = [...todoItem, name];
+    setTodoItem(copyTodoItem);
+    setName("")
+  }
+  const deleteItem = (index) =>{
+    let copyTodoItem = [...todoItem]
+    copyTodoItem.splice(index ,1);
+    setTodoItem(copyTodoItem);
+  }
+  const updateItem = (e,i) =>{
+    let copyTodoItem = [...todoItem]
+    copyTodoItem.splice(i,1,e.target.value);
+    setTodoItem(copyTodoItem);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>Todo List </h3>
+      <input type="text" placeholder="Enter item name " value= {name} onChange = {(e)=>setName(e.target.value)} />
+      <button onClick={addItem}>Add item </button>
+      <br /><br />
+      <ol>
+        {
+          todoItem.map((item,i)=>(
+            <div>
+              <span key={i}> {i + 1} {item} </span>
+              <button onClick={()=>deleteItem(i)}>delete</button>
+              <input type="text" placeholder="update item" onChange={(e)=>updateItem(e,i)} />
+            </div>
+          ))
+        }
+      </ol>
+    </>
+  )
 }
 
-export default App;
+export default App
